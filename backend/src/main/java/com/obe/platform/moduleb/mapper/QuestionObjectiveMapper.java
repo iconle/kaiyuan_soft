26fa7b63aa7af4fdf5057fbffc4136bc4118d1fa
qzb <1306561049@qq.com>
2026-05-27 22:25:36 +0800

    feature: 实现成绩录入和导入功能 #39 实现课程主讲教师课程大纲模块 #40
    - 作者：邱志斌
    - 时间：2026-05-27 22:25

---
+++ backend/src/main/java/com/obe/platform/moduleb/mapper/QuestionObjectiveMapper.java
@@ -0,0 +1,9 @@
+package com.obe.platform.moduleb.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.obe.platform.moduleb.entity.QuestionObjective;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionObjectiveMapper extends BaseMapper<QuestionObjective> {
+}
